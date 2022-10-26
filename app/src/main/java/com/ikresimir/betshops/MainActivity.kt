bf591c0a69544227a6e444d37b258398138a5bc1
kresohr <48351282+kresohr@users.noreply.github.com>
2022-10-27 00:29:12 +0200

    added: centerMapOnUser(), check for enabled location, gps_dialog, added broadcast receiver for gps changes

--- app/src/main/java/com/ikresimir/betshops/MainActivity.kt
+++ app/src/main/java/com/ikresimir/betshops/MainActivity.kt
@@ -3,30 +3,28 @@ package com.ikresimir.betshops
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Gravity
 import android.view.ViewGroup
 import android.view.Window
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-
+import androidx.core.location.LocationManagerCompat
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
-import com.google.android.gms.tasks.CancellationToken
-import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.android.gms.tasks.OnTokenCanceledListener
 
 
 class MainActivity : AppCompatActivity(),
@@ -44,9 +42,13 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(!isLocationEnabled(this)){
+            askForLocationDialog()
+        }
+
         val buttonSheet: Button = findViewById(R.id.btnTest)
         buttonSheet.setOnClickListener {
-            showDialog()
+            showLocationDetailsDialog()
         }
 
         val mapFragment: SupportMapFragment =
@@ -54,33 +56,57 @@ class MainActivity : AppCompatActivity(),
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                if (location != null) {
-                    val currentLatLong = LatLng(location.latitude, location.longitude)
-                    placeUserLocationMarker(currentLatLong)
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
-                }
-            }
-
+        centerMapOnUser()
     }
 
-    private fun showDialog() {
-        val dialog: Dialog = Dialog(this)
+    private fun askForLocationDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.gps_dialog)
+
+        val yesButton: TextView = dialog.findViewById(R.id.btnYes)
+        val noButton: TextView = dialog.findViewById(R.id.btnNo)
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.CENTER)
+
+        noButton.setOnClickListener {
+            dialog.dismiss()
+        }
+        yesButton.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            dialog.dismiss()
+        }
+    }
+
+    private fun showLocationDetailsDialog() {
+        val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.bottom_sheet)
 
-//        val editLayout: LinearLayout = dialog.findViewById(R.id.layoutEdit)
-//        val textPlaceholder: LinearLayout = dialog.findViewById(R.id.txtPlaceHolder)
-//        val text1: LinearLayout = dialog.findViewById(R.id.txtTest1)
-//        val txtRoute: TextView = dialog.findViewById(R.id.txtRoute)
+        val closeButton: ImageView = dialog.findViewById(R.id.btnClose)
+        val routeButton: TextView = dialog.findViewById(R.id.txtRoute)
 
         dialog.show()
         dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
         dialog.window?.setGravity(Gravity.BOTTOM)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.RED))
         dialog.window?.setDimAmount(0F)
+        dialog.window?.setBackgroundDrawableResource(R.color.white)
+        closeButton.setOnClickListener {
+            dialog.dismiss()
+        }
+        routeButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:44.787197,20.457273?q=44.787197,20.457273"+" ("+"test"+")"))
+            startActivity(intent)
+        }
+    }
+
+    private fun isLocationEnabled(context: Context): Boolean {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(locationManager)
     }
 
     private fun addMarkers(googleMap: GoogleMap){
@@ -93,11 +119,58 @@ class MainActivity : AppCompatActivity(),
         )
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        if(isLocationEnabled(this)){
+            Toast.makeText(this,"TESTING: Location is enabled", Toast.LENGTH_SHORT).show()
+            centerMapOnUser()
+        }
+
+        //Testing broadcast
+        val filter = IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION)
+        filter.addAction(Intent.ACTION_PROVIDER_CHANGED)
+        this.registerReceiver(gpsSwitchStateReceiver, filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        this.unregisterReceiver(gpsSwitchStateReceiver)
+    }
+
+    private val gpsSwitchStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (LocationManager.PROVIDERS_CHANGED_ACTION == intent.action) {
+                val locationManager = context.getSystemService(LOCATION_SERVICE) as LocationManager
+                val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                val isNetworkEnabled =
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+                if (isGpsEnabled && isNetworkEnabled) {
+                    centerMapOnUser()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun centerMapOnUser(){
+
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_LOW_POWER, null).addOnSuccessListener { location : Location? ->
+            if (location != null) {
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+                placeUserLocationMarker(currentLatLong)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
+                Toast.makeText(baseContext,"TESTING: MAP CENTERED ON USER", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
     @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
 
         map = googleMap
-        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isMyLocationButtonEnabled = false
         map.setOnMarkerClickListener(this)
         setUpMap()
         addMarkers(map)
@@ -128,20 +201,11 @@ class MainActivity : AppCompatActivity(),
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
             ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)   {
             map.isMyLocationEnabled = true
-
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location : Location? ->
-                    if (location != null) {
-                        val currentLatLong = LatLng(location.latitude, location.longitude)
-                        placeUserLocationMarker(currentLatLong)
-                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
-                    }
-                }
-
+            centerMapOnUser()
             return
         }
         else{
-            Toast.makeText(this,"Enable location in app settings for precise", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"TESTING: Location not enabled yet.", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -152,18 +216,5 @@ class MainActivity : AppCompatActivity(),
     }
 
     override fun onMarkerClick(p0: Marker) = false
-
-//    override fun onMyLocationButtonClick(): Boolean {
-//        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
-//            .show();
-//        // Return false so that we don't consume the event and the default behavior still occurs
-//        // (the camera animates to the user's current position).
-//        return false;
-//    }
-//
-//    override fun onMyLocationClick(location : Location) {
-//        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG)
-//            .show();
-//    }
 
 }
